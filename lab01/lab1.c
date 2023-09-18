eba101a7037573a2b4d0898a26af0b0a801d453f
user <user@ws1013402143.pm>
2023-09-18 15:30:53 +0500

    wip lab01

---
+++ lab01/lab1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, s;
+    printf("Введите a -> ");
+    scanf("%d", &a);
+    printf("Введите b -> ");
+    scanf("%d", &b);
+    s = a/10.0 + a%10;
+    if (s > b)
+        printf("%d\n", s);
+    if (s < b)
+        printf("%d\n", s + b);
+    if (s == b)
+    printf("Сумма цифр = %d\n", b);
+}
