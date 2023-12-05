b4fd01d56242ac43feb45673d39d565d2320a308
chertila0 <ancka.bondarencko2018@gmail.com>
2023-12-05 22:52:37 +0500

    math2

--- math1/mathlab1.py
+++ math1/mathlab1.py
@@ -1,27 +1,48 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sympy import *
+from sympy import Symbol,tanh,pi,limit
 
 
-def pic1():
-    x = [round(i, 2) for i in np.arange(-20, 21, 0.1)]
-    y = [round((i/(i-2)),2) for i in x]
-    plt.plot(x, y, linewidth=2, color='red')
-    plt.title('График функции x/(x-2)', fontsize=15)
-    plt.xlabel('x', fontsize=14)
-    plt.ylabel('y', fontsize=14) 
+#1
+# def pic1():
+#     x = [round(i, 2) for i in np.arange(-20, 21, 0.1)]
+#     y = [round((i/(i-2)),2) for i in x]
+#     plt.plot(x, y, linewidth=2, color='red')
+#     plt.title('График функции x/(x-2)', fontsize=15)
+#     plt.xlabel('x', fontsize=14)
+#     plt.ylabel('y', fontsize=14) 
+#     plt.grid(True)
+# def pic2():
+#     x = np.linspace(-15,11,1000)
+#     y = np.arcsin(2/(x+3))
+#     plt.plot(x, y, linewidth=2, color='blue')
+#     plt.title('График функции arcsin(2/(x+3))', fontsize=15)
+#     plt.xlabel('x', fontsize=14)
+#     plt.ylabel('y', fontsize=14) 
+#     plt.grid(True)
+
+# plt.figure()
+# pic1()
+# plt.figure()
+# pic2()
+# plt.show()
+
+#2
+def pic():
+    x = np.linspace(-5*np.pi, 5*np.pi, 1000)
+    y = x * np.tan(x) / (np.pi - x)
+    n = Symbol('n')
+    x1 = limit(n * tanh(n) / (pi - n), n, pi)
+    print(x1)
+    
+    plt.plot(x1)
     plt.grid(True)
-def pic2():
-    x = np.arange(-100,100,0.1)
-    y = np.arcsin(2/(x+3))
-    plt.plot(x, y, linewidth=2, color='blue')
-    plt.title('График функции arcsin(2/(x+3))', fontsize=15)
-    plt.xlabel('x', fontsize=14)
-    plt.ylabel('y', fontsize=14) 
-    plt.grid(True)
+    plt.show()
 
-plt.figure()
-pic1()
-plt.figure()
-pic2()
-plt.show()
+pic()
+
+
+
+
+
+
