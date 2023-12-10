db1bb6cd593bd5576460b278463d8cf4949c9737
chertila0 <ancka.bondarencko2018@gmail.com>
2023-12-11 00:52:45 +0500

    wip lab08

--- lab08/lab8.py
+++ lab08/lab8.py
@@ -1,14 +1,16 @@
 def make_calc(operation, a):
-    
     def cilc(b):
+        nonlocal a
         if operation == '+':
-            return a + b
+            a += b
         elif operation == '-':
-            return a - b
+            a -= b
         elif operation == '*':
-            return a * b
+            a *= b
         elif operation == '/':
-            return round(a / b, 2)
+            a =  round(a / b, 2)
+        return a
     return cilc
 calc = make_calc('+', a=1)
 print(calc(5))
+print(calc(7))
