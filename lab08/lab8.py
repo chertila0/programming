e93247dffb60bd25baa7f597912b961c3a7b5d51
chertila0 <ancka.bondarencko2018@gmail.com>
2023-12-17 23:46:29 +0500

    halp me pls

--- lab08/lab8.py
+++ lab08/lab8.py
@@ -1,3 +1,27 @@
+from functools import wraps
+
+
+
+def f(n):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            results = []
+            for _ in range(n):
+                result = func(*args, **kwargs)
+                results.append(result)
+            return results
+        return wrapper
+    return decorator
+
+# def hg(n):
+#     def gh(m):
+#         return n*m
+#     return gh
+# @f(4)
+# def g(x,y,):
+#     return hg(x)(y)
+# print(g(5,6))
 def make_calc(operation, a):
     def cilc(b):
         nonlocal a
@@ -11,6 +35,11 @@ def make_calc(operation, a):
             a =  round(a / b, 2)
         return a
     return cilc
-calc = make_calc('+', a=1)
-print(calc(5))
-print(calc(7))
+@f(4)
+def calc(x,y,z):
+    y = make_calc(x,y)(z)
+    return make_calc(x,y)(z)
+print(calc('+', 1, 3))
+
+
+
