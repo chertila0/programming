3788969ee135082495ed208f5dfc4c0e581fe5cd
user <user@ws1013402139.pm>
2023-12-18 12:11:26 +0500

    halp me

--- lab09/lab9.py
+++ lab09/lab9.py
@@ -4,3 +4,7 @@ def rand_num(min_val, max_val):
     seed = int(time.time() * 1000)
     random_num = (seed % (max_val - min_val + 1)) + min_val
     return random_num
+
+mins = 20
+maxs = 70 
+print(rand_num(mins, maxs))
