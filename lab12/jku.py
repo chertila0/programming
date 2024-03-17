e70ca2ff69c9a464f1280dbb9a576be145612f58
chertila0 <ancka.bondarencko2018@gmail.com>
2024-03-17 22:11:16 +0500

    lab12

--- lab12/jku.py
+++ lab12/jku.py
@@ -1,12 +1,14 @@
 from appJar import gui
 from triangle import triangle as tri
+from rectangle import rectangle as rect
+from trapezoid import trapezoid as trap
 
 app=gui()
 
 
 app.setBg('light blue')
 app.addLabel('Здравствуйте! Какую фигуру вы хотите выбрать?')
-app.addLabelOptionBox('Фигура',['Треугольник','Трапеция','Квадрат'])
+app.addLabelOptionBox('Фигура',['Треугольник','Трапеция','Прямоугольник'])
 app.addLabel('Отлично, теперь выберите что вы хотите посчитать:')
 app.addLabelOptionBox('Параметр', ['Площадь фигуры','Радиус описанной окружности','Радиус вписанной окружности'])
 
@@ -16,13 +18,23 @@ def press(win):
         
 
 app.startSubWindow('Продолжить')
+app.addLabel('Введите стороны выбранной фигуры.')
+app.addLabel('Подсказка: для прямоугольника достаточно 2-х строн, для треугольника необходимо ввести все 3 стороны,')
+app.addLabel(' для трапеции необходимо ввести все 4 стороны.')
 app.addNumericEntry('Сторона 1')
 app.addNumericEntry('Сторона 2')
 app.addNumericEntry('Сторона 3')
 app.addNumericEntry('Сторона 4')
 def result(res):
-    if app.getEntry('Сторона 3') == None and app.getEntry('Сторона 4') == None:
-        pass
+    if app.getEntry('Сторона 1') == None or app.getEntry('Сторона 2') == None or app.getEntry('Сторона 3') == None and app.getEntry('Сторона 4') != None:
+        res = 'Кажется вы ошиблись при вводе сторон! Подсказка: вводите стороны по порядку, не пропуская поля ввода.'
+    elif app.getEntry('Сторона 3') == None and app.getEntry('Сторона 4') == None:
+        if app.getOptionBox('Параметр') == 'Площадь фигуры':
+            res = rect(app.getEntry('Сторона 1'), app.getEntry('Сторона 2')).ploshad()
+        elif app.getOptionBox('Параметр') == 'Радиус описанной окружности':
+            res = rect(app.getEntry('Сторона 1'), app.getEntry('Сторона 2')).rad_opis()
+        else:
+            res = rect(app.getEntry('Сторона 1'), app.getEntry('Сторона 2')).rad_vpis()
     elif app.getEntry('Сторона 4') == None:
         if app.getOptionBox('Параметр') == 'Площадь фигуры':
             res = tri(app.getEntry('Сторона 1'), app.getEntry('Сторона 2'), app.getEntry('Сторона 3')).ploshad()
@@ -30,7 +42,15 @@ def result(res):
             res = tri(app.getEntry('Сторона 1'), app.getEntry('Сторона 2'), app.getEntry('Сторона 3')).rad_opis()
         else:
             res = tri(app.getEntry('Сторона 1'), app.getEntry('Сторона 2'), app.getEntry('Сторона 3')).rad_vpis()
-        app.infoBox('', f'Результат:{res}')
+    else:
+        if app.getOptionBox('Параметр') == 'Площадь фигуры':
+            res = trap(app.getEntry(''), app.getEntry(''), app.getEntry(''), app.getEntry('')).ploshad()
+        elif app.getOptionBox('Параметр') == 'Радиус описанной окружности':
+            res = trap(app.getEntry(''), app.getEntry(''), app.getEntry(''), app.getEntry('')).rad_opis()
+        else:
+            res = trap(app.getEntry(''), app.getEntry(''), app.getEntry(''), app.getEntry('')).rad_vpis()
+    app.hideSubWindow(win)
+    app.infoBox('', f'Результат:{res}')
 app.addButton('Рассчитать', result)
 app.stopSubWindow()
 app.addButton('Продолжить', press)
