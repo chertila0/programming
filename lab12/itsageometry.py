2a9117310b5f3d83b5f7eccca4beb2626e304db5
user <user@ws1013402139.pm>
2024-04-22 12:44:55 +0500

    laba12

--- lab12/itsageometry.py
+++ lab12/itsageometry.py
@@ -12,8 +12,7 @@ app.addLabelOptionBox('Фигура',['Треугольник','Трапеция
 app.addLabel('Отлично, теперь выберите что вы хотите посчитать:')
 app.addLabelOptionBox('Параметр', ['Площадь фигуры','Радиус описанной окружности','Радиус вписанной окружности'])
 
-type_figure = ''
-type_operation =''
+
 def press(win):
     app.showSubWindow(win)
 
@@ -28,60 +27,12 @@ app.addNumericEntry('Сторона 1')
 app.addNumericEntry('Сторона 2')
 app.addNumericEntry('Сторона 3')
 app.addNumericEntry('Сторона 4')
-def result(res):
-    type_operation = app.getOptionBox('Параметр')
-    type_figure = app.getOptionBox('Фигура')
-    storona_1 = app.getEntry('Сторона 1')
-    storona_2 = app.getEntry('Сторона 2')
-    storona_3 = app.getEntry('Сторона 3')
-    storona_4 = app.getEntry('Сторона 4')
-    res = 'пусто'
-    if type_operation == 'Площадь фигуры' and type_figure == 'Прямоугольник':
-        if storona_1 != None and storona_2 != None and storona_3 == None and storona_4 == None:
-            res = rect(storona_1, storona_2,storona_3,storona_4).ploshad()
-        else: 
-            res = "Вы ошиблись при вводе сторон! Подсказка: вводите стороны по порядку"
-    if type_operation == 'Площадь фигуры' and type_figure == 'Треугольник':
-        if storona_1 != None and storona_2 != None and storona_3 != None and storona_4 == None:
-            res = tri(storona_1,storona_2,storona_3,storona_4).ploshad()
-        else:
-            res = "Вы ошиблись при вводе сторон! Подсказка: вводите стороны по порядку"
-    if type_operation == 'Площадь фигуры' and type_figure == 'Трапеция':
-        if storona_1 != None and storona_2 != None and storona_3 != None and storona_4 != None:
-            res = trap(storona_1,storona_2,storona_3,storona_4).ploshad()
-        else:
-            res = "Вы ошиблись при вводе сторон! Подсказка: вводите стороны по порядку"
-    if type_operation == 'Радиус описанной окружности' and type_figure == 'Прямоугольник':
-        if storona_1 != None and storona_2 != None and storona_3 == None and storona_4 == None:
-            res = rect(storona_1,storona_2,storona_3,storona_4).rad_opis()
-        else:
-            res = "Вы ошиблись при вводе сторон! Подсказка: вводите стороны по порядку"
-    if type_operation == 'Радиус описанной окружности' and type_figure == 'Треугольник':
-        if storona_1 != None and storona_2 != None and storona_3 != None and storona_4 == None:
-            res = tri(storona_1,storona_2,storona_3,storona_4).rad_opis()
-        else:
-            res = "Вы ошиблись при вводе сторон! Подсказка: вводите стороны по порядку"
-    if type_operation == 'Радиус описанной окружности' and type_figure == 'Трапеция':
-        if storona_1 != None and storona_2 != None and storona_3 != None and storona_4 != None:
-            res = trap(storona_1, storona_2, storona_3,storona_4).rad_opis()
-    if type_operation == 'Радиус вписанной окружности' and type_figure == 'Прямоугольник':
-        if storona_1 != None and storona_2 != None and storona_3 == None and storona_4 == None:
-            res = rect(storona_1,storona_2,storona_3,storona_4).rad_vpis()
-        else:
-            res = "Вы ошиблись при вводе сторон! Подсказка: вводите стороны по порядку"
-    if type_operation == 'Радиус вписанной окружности' and type_figure == 'Треугольник':
-        if storona_1 != None and storona_2 != None and storona_3 != None and storona_4 == None:
-            res = tri(storona_1,storona_2,storona_3,storona_4).rad_vpis()
-        else:
-            res = "Вы ошиблись при вводе сторон! Подсказка: вводите стороны по порядку"
-    if type_operation == 'Радиус вписанной окружности' and type_figure == 'Трапеция':
-        if storona_1 != None and storona_2 != None and storona_3 != None and storona_4 != None:
-            res = trap(storona_1,storona_2,storona_3,storona_4).rad_vpis()
-        else:
-            res = "Вы ошиблись при вводе сторон! Подсказка: вводите стороны по порядку"
-    app.infoBox('',f'Результат: {res}')
 
-app.addButton('Рассчитать', result)
+app.startSubWindow("Рассчитать")
+app.addLabel('Я молодец')
+app.stopSubWindow()
+
+app.addButton('Рассчитать', press)
 app.stopSubWindow()
 app.addButton('Продолжить',press)
 app.go()
